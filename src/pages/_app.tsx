f8bcf15d7c3cea6156fe39cceac42260e60af5a1
365support <365support@naver.com>
2023-07-30 16:01:26 +0900

    build: GlobalStyle setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import GlobalStyle from "@/styles/GlobalStyle";
 
 export const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
         <Component {...pageProps} />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
