cb88b1b79ac98525153c0fd1eed605af3a8b07e4
365support <365support@naver.com>
2023-07-30 22:25:22 +0900

    feat: 로그인 페이지 완성

---
+++ src/domains/login/index.tsx
@@ -0,0 +1,50 @@
+import { css } from "@emotion/react";
+import LoginWithKakao from "./LoginWithKakao";
+import CenteredLayout from "../shared/layout/CenteredLayout";
+import MainLogo from "../shared/MainLogo";
+
+const LoginPage = () => {
+  return (
+    <CenteredLayout>
+      <main css={homeContainer}>
+        <div css={logoPosition}>
+          <MainLogo />
+        </div>
+        <div css={loginPosition}>
+          <LoginWithKakao />
+        </div>
+      </main>
+    </CenteredLayout>
+  );
+};
+
+const homeContainer = css`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  background: linear-gradient(
+    134deg,
+    #c4a1d7 9.81%,
+    #86b3cc 39.06%,
+    #8cbbd4 45.31%,
+    #fff3ea 85.94%,
+    #fff387 100%
+  );
+`;
+
+const logoPosition = css`
+  position: absolute;
+  top: 15%;
+  width: 26vh;
+  height: 26vh;
+`;
+
+const loginPosition = css`
+  position: absolute;
+  bottom: 15%;
+  width: 75%;
+`;
+
+export default LoginPage;
---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import LoginPage from "@/domains/login";
+
+const Login: NextPage = () => {
+  return <LoginPage />;
+};
+
+export default Login;
