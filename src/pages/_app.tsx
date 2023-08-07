367e0b1696d30b48ab5208a8f41da6fb324601e2
365support <365support@naver.com>
2023-08-07 20:13:03 +0900

    build: @toss/use-overlay setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { OverlayProvider } from "@toss/use-overlay";
 import GlobalStyle from "@/styles/GlobalStyle";
 
 export const queryClient = new QueryClient();
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
-        <Component {...pageProps} />
+        <OverlayProvider>
+          <Component {...pageProps} />
+        </OverlayProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
